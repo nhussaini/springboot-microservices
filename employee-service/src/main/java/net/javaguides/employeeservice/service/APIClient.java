73aec4e4bf1ce023c0ca51b533a89f05ca4ae5c9
nhussaini <nasrullah_hussaini@yahoo.com>
2023-02-25 19:50:51 -0500

    create and set up API Gateway microservice

--- employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//use Eureka service registry and discovery
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
