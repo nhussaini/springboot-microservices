26bca94c3b4f9fd9aca13b062b1d2a627ace370f
nhussaini <nasrullah_hussaini@yahoo.com>
2023-02-28 13:59:13 -0500

    build save organization rest api

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/controller/OrganizationController.java
@@ -0,0 +1,25 @@
+package net.javaguides.organizationservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.organizationservice.dto.OrganizationDto;
+import net.javaguides.organizationservice.service.OrganizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/organizations")
+@AllArgsConstructor
+public class OrganizationController {
+    private OrganizationService organizationService;
+
+    //Build save Organization Rest API
+    @PostMapping
+    public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
+        OrganizationDto savedOrganization = organizationService.saveOrganization(organizationDto);
+        return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
+    }
+}
